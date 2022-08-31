5c5ec8a9658085789d62dda46f4123d0a4c2bd0e
Matt Jewell <m@mattjewell.co.uk>
2022-08-31 14:05:44 +0200

    from this github documentation…

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,11 @@ import { AppService } from './app.service';
       useFactory: (configService: ConfigService) => {
         const config: TypeOrmModuleOptions = {
           type: 'postgres',
-          url: configService.get('DATABASE_URL'),
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DATABASE'),
           ssl: false,
           synchronize: true,
         };
