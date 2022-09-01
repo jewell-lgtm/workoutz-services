ae63ac338122ccd9404da51e2f41425ca4b23bf0
Matt Jewell <m@mattjewell.co.uk>
2022-09-01 16:19:52 +0200

    added a script that creates an environment file

---
+++ create-db-config.ts
@@ -0,0 +1,19 @@
+import { writeFileSync } from "node:fs"
+
+function env(key: string): string {
+	const result = process.env[key]
+	if (!result) {
+		throw new Error(`Required env variable ${key} is not set`)
+	}
+	return result
+}
+
+writeFileSync(
+	".env",
+	`POSTGRES_HOST=${env("POSTGRES_HOST")}
+POSTGRES_USER=${env("POSTGRES_USER")}
+POSTGRES_PASSWORD=${env("POSTGRES_PASSWORD")}
+POSTGRES_DATABASE=${env("POSTGRES_DATABASE")}
+POSTGRES_PORT=${env("POSTGRES_PORT")}
+`,
+)
